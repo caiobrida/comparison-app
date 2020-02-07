fd2f6ed06991d25c324c83cd6494e35512d76c1f
caiobrida <caiobrida1@gmail.com>
2020-02-07 15:07:04 -0300

    added environment variables

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,12 +1,41 @@
-module.exports = {
-    dialect: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '',
-    database: 'comparison_database',
-    define: {
-        timestamps: true,
-        underscored: true
+require('dotenv').config();
+
+const config = {
+    "development": {
+      "username": "root",
+      "password": null,
+      "database": "comparison_database",
+      "host": "127.0.0.1",
+      "dialect": "mysql",
+      "define": {
+        "timestamps": true,
+        "underscored": true
+      }
+    },
+    "test": {
+      "username": "root",
+      "password": null,
+      "database": "comparison_database",
+      "host": "127.0.0.1",
+      "dialect": "mysql",
+      "define": {
+        "timestamps": true,
+        "underscored": true
+      }
+    },
+    "production": {
+      "username": process.env.USERNAME,
+      "password": process.env.PASSWORD,
+      "database": process.env.DB,
+      "host": process.env.HOST,
+      "dialect": "mysql",
+      "define": {
+        "timestamps": true,
+        "underscored": true
+      }
     }
 }
+
+const currEnv = process.env.NODE_ENV
+
+module.exports = config[currEnv];
--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize');
 
 const dbConfig = require('../config/database');
-
 const User = require('../models/User');
 
 const connection = new Sequelize(dbConfig);
